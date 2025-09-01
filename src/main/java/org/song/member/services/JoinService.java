d9f84feec31f95de06bfb9c20358c9222b632918
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-09-01 13:22:20 +0900

    확인

--- src/main/java/org/song/member/services/JoinService.java
+++ src/main/java/org/song/member/services/JoinService.java
@@ -1,13 +1,14 @@
 package org.song.member.services;
 
 import lombok.RequiredArgsConstructor;
+import org.song.member.controller.RequestJoin;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class JoinService {
 
-    public void process(){
+    public void process(RequestJoin form){
 
     }
 }
--- src/main/java/org/song/member/validator/JoinValidator.java
+++ src/main/java/org/song/member/validator/JoinValidator.java
@@ -1,8 +1,10 @@
 package org.song.member.validator;
 
+import org.song.member.controller.RequestJoin;
+import org.springframework.validation.Errors;
+
 public class JoinValidator {
 
-    public void process(){
-
+    public void process(RequestJoin form, Errors errors) {
     }
 }
