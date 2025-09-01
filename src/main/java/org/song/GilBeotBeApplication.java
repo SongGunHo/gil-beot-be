69c898ee7abeab6b58c42e3a31903e0c053ef6d0
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-09-01 12:20:56 +0900

    설정중

---
+++ src/main/java/org/song/GilBeotBeApplication.java
@@ -0,0 +1,13 @@
+package org.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilBeotBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GilBeotBeApplication.class, args);
+	}
+
+}
